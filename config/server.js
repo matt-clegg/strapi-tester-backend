d7b6b18a9a8dd279e6d164306da0443230751d01
Matt <mattclegg93@gmail.com>
2021-04-14 18:53:34 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '67ab098caf22f02068b45598cf19678e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fbab5f81-1dca-4d07-83cd-db68b270794d'
+};
